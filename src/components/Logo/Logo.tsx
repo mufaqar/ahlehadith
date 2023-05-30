ad046c15b7220268cf18814e286a4201ca35e688
mufaqar <mufaqar@gmail.com>
2023-05-30 18:31:41 +0500

    Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,8 +5,8 @@ import React, { useEffect } from "react";
 const Logo = () => {
 
   return (
-    <Link href="/" className="w-36 md:w-52">
-      <Image src="/assets/images/ahlehadith-logo.png" alt="logo" width={160} height={60} className="w-auto h-auto" />
+    <Link href="/" className="w-36 md:w-52 bg-white rounded-xl ">
+      <Image src="/assets/images/logo.png" alt="logo" width={160} height={50} className="w-auto h-auto" />
       {/* <Image src="/assets/images/black-logo.png" alt="logo" width={150} height={60}/> */}
     </Link>
   );
