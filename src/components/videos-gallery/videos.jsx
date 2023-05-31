712fdbb24263ab7d207471c0ea5506461ffc931d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-05-31 14:27:05 +0500

    Videos

--- src/components/videos-gallery/videos.jsx
+++ src/components/videos-gallery/videos.jsx
@@ -16,20 +16,53 @@ const VideosGallery = () => {
     <>
       <section className="md:flex gap-6">
         <div className="rounded-3xl relative md:w-[77%] overflow-hidden inline-block border-[10px] border-white dark:border-transparent shadow-xl">
-          <Image
+          {/* <Image
             src="/assets/images/thumbnil1.jpg"
             alt="thumbnil"
             width={900}
             height={900}
             className="w-full h-full object-cover"
-          />
+          /> */}
+          {Video_Data.slice(0, 1).map((item, idx) => {
+            return (
+              <div key={idx} className="player-wrapper w-full h-full object-cover">
+                <ReactPlayer
+                  className="react-player w-full h-full object-cover"
+                  url={item.url}
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+
+            );
+          })}
           <BsPlayCircle
             onClick={openModal}
             className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-yellow active:scale-105 animate-pulse text-7xl"
           />
         </div>
-        <div className="flex md:flex-col mt-5 md:mt-0 justify-between gap-5 md:w-[23%]">
-          <div className="rounded-2xl relative md:rounded-3xl h-[33%] border-[6px] border-white dark:border-transparent shadow-xl overflow-hidden inline-block">
+        <div className="flex flex-col mt-5 md:mt-0 justify-between gap-5 md:w-[23%]">
+          {Video_Data.slice(0, 3).map((item, idx) => {
+            return (
+              <div key={idx} className="rounded-2xl relative md:rounded-3xl h-[33%] border-[6px] border-white dark:border-transparent shadow-xl overflow-hidden inline-block">
+                <div className="player-wrapper w-full h-full object-cover">
+                  <ReactPlayer
+                    className="react-player w-full h-full object-cover"
+                    url={item.url}
+                    width="100%"
+                    height="100%"
+                  />
+                </div>
+
+
+                <BsPlayCircle
+                  onClick={openModal}
+                  className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-yellow active:scale-105 animate-pulse text-5xl"
+                />
+              </div>
+            );
+          })}
+          {/* <div className="rounded-2xl relative md:rounded-3xl h-[33%] border-[6px] border-white dark:border-transparent shadow-xl overflow-hidden inline-block">
             <Image
               src="/assets/images/thumbnil1.jpg"
               alt="thumbnil"
@@ -54,20 +87,7 @@ const VideosGallery = () => {
               onClick={openModal}
               className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-yellow active:scale-105 animate-pulse text-5xl"
             />
-          </div>
-          <div className="rounded-2xl relative md:rounded-3xl h-[33%] border-[6px] border-white dark:border-transparent shadow-xl overflow-hidden inline-block">
-            <Image
-              src="/assets/images/thumbnil1.jpg"
-              alt="thumbnil"
-              width={900}
-              height={900}
-              className="w-full h-full object-cover"
-            />
-            <BsPlayCircle
-              onClick={openModal}
-              className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-yellow active:scale-105 animate-pulse text-5xl"
-            />
-          </div>
+          </div> */}
         </div>
       </section>
       {modalIsOpen && <VideoModelBox />}
@@ -105,7 +125,7 @@ const VideoModelBox = () => {
         <div className="player-wrapper">
           <ReactPlayer
             className="react-player"
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            url="https://www.youtube.com/watch?v=DASJPMM-LSs"
             width="100%"
             height="100%"
           />
@@ -114,3 +134,15 @@ const VideoModelBox = () => {
     </div>
   );
 };
+
+export const Video_Data = [
+  {
+    url: "https://www.youtube.com/watch?v=DASJPMM-LSs",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=vzElLps3GQI",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=QfOGNxFvgtY",
+  },
+];
