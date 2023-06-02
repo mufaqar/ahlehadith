f9a1fee2200b15fcb84a384a1a25d9f4af19a3d5
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-06-02 19:28:53 +0500

    Gallery Slider

--- src/components/images-section.tsx
+++ src/components/images-section.tsx
@@ -11,6 +11,29 @@ const ImagesSection = () => {
         slidesToShow: 4,
         slidesToScroll: 1,
         arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     };
     const slider = React.useRef<Slider>(null);
 
