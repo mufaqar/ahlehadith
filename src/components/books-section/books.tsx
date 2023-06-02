0237d869e1e51c5ef8e0817e2cd61b30948f27c3
mufaqar <mufaqar@gmail.com>
2023-06-02 20:29:24 +0500

    updated

--- src/components/books-section/books.tsx
+++ src/components/books-section/books.tsx
@@ -22,8 +22,8 @@ const BooksSection = () => {
                                 </Link>
                             </div>
                         </div>
-                        <div className="overlay bg-black/30 -z-0 absolute top-0 left-0 h-full w-full before:content-[' '] before:absolute before:top-3 before:bottom-3 before:left-3 before:right-3 before:border-white before:border-y before:scale-x-0 group-hover:before:scale-x-100 before:transform before:transition before:duration-700 before:ease-in-out 
-                                 after:content-[' '] after:absolute after:top-3 after:bottom-3 after:left-3 after:right-3 after:border-white after:border-x after:scale-y-0 group-hover:after:scale-y-100 after:transform after:transition after:duration-700 after:ease-in-out "
+                        <div className="overlay bg-black/30 -z-0 absolute top-0 left-0 h-full w-full before:content-[' '] before:absolute before:top-3 before:bottom-3 before:left-3 before:right-3 before:border-yellow before:border-y before:scale-x-0 group-hover:before:scale-x-100 before:transform before:transition before:duration-700 before:ease-in-out 
+                                 after:content-[' '] after:absolute after:top-3 after:bottom-3 after:left-3 after:right-3 after:border-yellow after:border-x after:scale-y-0 group-hover:after:scale-y-100 after:transform after:transition after:duration-700 after:ease-in-out "
                         ></div>
                     </div>
                 );
