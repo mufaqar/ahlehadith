8c9b79cece121b8f1a9c4f6f7b5d478cfcc87084
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-06-14 11:07:21 +0500

    Input

--- src/components/controlls/input.tsx
+++ src/components/controlls/input.tsx
@@ -7,7 +7,7 @@ const Input = ({ searchValue, handleChange, className }: any) => {
       value={searchValue}
       className={`p-2 outline-none border-none shadow-none focus:outline-none ${className}`}
       onChange={(e) => handleChange(e.target.value)}
-      placeholder="ای میل ایڈریس"
+      placeholder="تلاش کریں"
     />
   );
 };
