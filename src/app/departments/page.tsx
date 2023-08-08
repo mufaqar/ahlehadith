02d411a720ac71d499ad4a3169b8ac423c77e9d5
mufaqar <mufaqar@gmail.com>
2023-08-08 13:56:06 +0500

    updated

--- src/app/departments/page.tsx
+++ src/app/departments/page.tsx
@@ -4,6 +4,11 @@ import { useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import ModelBox from '../../components/ModelBox/ModelBox'
 
+interface IGallery {
+img: string
+
+}
+
 const Page = () => {
   const columnsCountBreakPoints = { 200: 1, 280: 2, 900: 3 };
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -24,7 +29,7 @@ const Page = () => {
           <div className="my-10">
             <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
               <Masonry columnsCount={3} gutter="20px">
-                {Gallery.map((image) => (
+                {Gallery.map((image:IGallery) => (
                   <figure key={image.img} className="p-1 hover:shadow-lg cursor-pointer">
                     <img src={image.img} className="w-full rounded-xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]" onClick={() => OpenModelBox(image)} />
                   </figure>
