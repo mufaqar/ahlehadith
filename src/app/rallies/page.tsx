0e1fad2c9240992a5946c1e3e7179fa77d734881
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-15 16:10:01 +0500

    Ralies videos

--- src/app/rallies/page.tsx
+++ src/app/rallies/page.tsx
@@ -3,23 +3,31 @@ import React from 'react';
 import Layout from "@/components/Layout/Layout";
 import { Rallies_vides } from '@/const/exports';
 import { TVideo } from '@/utils/types';
+import YouTube, { YouTubeProps } from 'react-youtube';
+
 const Page = () => {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    event.target.pauseVideo();
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    playerVars: {
+      autoplay: 0,
+    },
+  };
 
   return (
     <>
       <Layout>
         <div className='items-center font-ahle my-10 md:my-20 md:mt-20 grid gap-10'>
           <div className="my-10 grid md:grid-cols-3 grid-cols-1 gap-7">
-            {Rallies_vides.map((item:TVideo, idx:number) => {
-              return <div key={idx} className="">
-                <iframe
-                  src={`https://www.youtube.com/embed/${item.url}`}
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow=""
-                  allowFullScreen
-                  className="w-full h-full aspect-square"></iframe>
-              </div>
+            {Rallies_vides.map((item: TVideo, idx: number) => {
+              return <YouTube key={idx}
+                videoId={item?.url}
+                opts={opts}
+                className={`videocontainer`}
+                iframeClassName={`w-full h-full aspect-square`}
+                onReady={onPlayerReady} />
             })}
           </div>
         </div>
