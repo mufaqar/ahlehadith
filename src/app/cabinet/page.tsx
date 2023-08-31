49feb642f84f425228dc48518c2e65d559f15025
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-08-31 15:50:32 +0500

    cabinet

--- src/app/cabinet/page.tsx
+++ src/app/cabinet/page.tsx
@@ -5,12 +5,128 @@ const Page = () => {
   return (
     <>
       <Layout>
-        <div className='items-center font-ahle my-10 md:my-20 md:mt-20 grid gap-10'>
+        <div className='my-10 md:my-20 md:mt-20'>
+          <div>
+            <h2 className="text-3xl uppercase font-ahle mb-10">
+              مرکزی جمعیت اہل حدیث پاکستان 2020ء
+            </h2>
+            <ul className="grid gap-5">
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                پروفیسر ساجد میر صاحب  امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                ڈاکٹر حافظ عبدالکریم صاحب  ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                حاجی محمد نواز مغل  ناظم مالیات
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا علی محمد ابوتراب  سینئر نائب امیر (امیر صوبہ بلوچستان)
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب مولانا یوسف انور نائب امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب حافظ ابتسام الٰہی ظہیر   چیف آرگنائزر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب پروفیسر حافظ عبدالستار حامد نائب امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب قاری صہیب احمد میر محمدی (نائب امیر)
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب مولانا مفتی یوسف محمدی  نائب امیر (امیر سندھ)
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا عبدالرشید حجازی صاحب  (نائب امیر)
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا حافظ عبدالحمید عامر صاحب   نائب امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا روح اللہ توحیدی صاحب  نائب امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب چوہدری کاشف نواز رندھاوا   نائب امیر و سیکرٹری اطلاعات
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب حافظ معتصم الٰہی ظہیر صاحب نائب امیرو چیئر مین سیاسی کمیٹی
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                ڈاکٹر عبدالغفور راشد صاحب  نائب امیر
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب حافظ محمد علی یزدانی   نائب ناظم اعلیٰ و نائب سیاسی کمیٹی
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                رانا محمد شفیق خاں پسروری صاحب   ایڈیشنل سیکرٹری جنرل
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا محمد نعیم بٹ صاحب    سینئر نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا قاری محمد حنیف ربانی صاحب  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                سید عتیق الرحمن شاہ صاحب  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا محمد یاسین راہی صاحب   نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                رانا محمد خلیق خاں پسروری صاحب  نائب ناظم اعلیٰ برائے رابطہ جنوبی پنجاب
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                قاضی ریاض قدیر صاحب  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب میاں محمد فضل حق ثانی نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب امتیاز احمد مجاہد  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب ڈاکٹر زعیم الدین لکھوی  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب مولانا محمد ابراہیم طارق  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب مولانا بنیامین عابد  نائب ناظم اعلیٰ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا ارشاد الحق اثری صاحب ناظم شعبہ تحقیق و افتاء
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                ڈاکٹر حماد لکھوی صاحب     ناظم شعبہ طبع و تالیف
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا عبدالباسط شیخوپوری صاحب ناظم شعبہ تبلیغ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                مولانا عبدالمالک مجاہد صاحب    ناظم خارجہ امور
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                حافظ شاہد امین صاحب    ناظم شعبہ خدمت خلق
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                ملک محمد سلیمان صاحب  نائب ناظم مالیات
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب میاں محمد راشد   نائب ناظم مالیات
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب حافظ فیصل افضل شیخ
+              </li>
+              <li className="font-ahle text-base text-gray-600 dark:text-text">
+                جناب حافظ بابر فاروق رحیمی   نائب ناظم تبلیغ
+              </li>
 
+            </ul>
+          </div>
         </div>
       </Layout>
-
-
     </>
   );
 };
