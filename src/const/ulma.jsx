c2c95f01da9fd7ac48e1f8fbaa71f85b4ad8edf7
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-09-11 19:32:50 +0500

    ulma-ikram

--- src/const/ulma.jsx
+++ src/const/ulma.jsx
@@ -1,138 +1,138 @@
 export const Ulma_Data = [
     {
         name: "حافظ مسعود عالم",
-        img: "/assets/images/ulma/1.jpg",
+        img: "/assets/images/ulma/1.JPG",
     },
     {
         name: "مفتی عبدالستار حماد",
-        img: "/assets/images/ulma/2.jpg",
+        img: "/assets/images/ulma/2.JPG",
     },
     {
         name: "مولانا ارشاد الحق اثری",
-        img: "/assets/images/ulma/3.jpg",
+        img: "/assets/images/ulma/3.JPG",
     },
     {
         name: "سید سبطین شاہ نقوی",
-        img: "/assets/images/ulma/4.jpg",
+        img: "/assets/images/ulma/4.JPG",
     },
     {
         name: "پروفیسر یسین ظفر",
-        img: "/assets/images/ulma/5.jpg",
+        img: "/assets/images/ulma/5.JPG",
     },
     {
         name: "قاری ابراہیم میر محمدی",
-        img: "/assets/images/ulma/6.jpg",
+        img: "/assets/images/ulma/6.JPG",
     },
     {
         name: "قاری عزیر احمد",
-        img: "/assets/images/ulma/7.jpg",
+        img: "/assets/images/ulma/7.JPG",
     },
     {
         name: "قاری صہیب احمد میر محمدی",
-        img: "/assets/images/ulma/8.jpg",
+        img: "/assets/images/ulma/8.JPG",
     },
     {
         name: "قاری یحییٰ رسول نگری۔",
-        img: "/assets/images/ulma/9.jpg",
+        img: "/assets/images/ulma/9.JPG",
     },
     {
         name: "قاری حنیف ربانی",
-        img: "/assets/images/ulma/10.jpg",
+        img: "/assets/images/ulma/10.JPG",
     },
     {
         name: "حافظ محمد شریف صاحب",
-        img: "/assets/images/ulma/11.jpg",
+        img: "/assets/images/ulma/11.JPG",
     },
     {
         name: "حافظ قسیم شیخوپوری",
-        img: "/assets/images/ulma/12.jpg",
+        img: "/assets/images/ulma/12.JPG",
     },
     {
         name: "ڈاکٹر عبدالغفور راشد",
-        img: "/assets/images/ulma/13.jpg",
+        img: "/assets/images/ulma/13.JPG",
     },
     {
         name: "مولانا ندیم ظہیر",
-        img: "/assets/images/ulma/14.jpg",
+        img: "/assets/images/ulma/14.JPG",
     },
     {
         name: "مولانا عبدالرشید حجازی",
-        img: "/assets/images/ulma/15.jpg",
+        img: "/assets/images/ulma/15.JPG",
     },
     {
         name: "مولانا ابرار ظہیر",
-        img: "/assets/images/ulma/16.jpg",
+        img: "/assets/images/ulma/16.JPG",
     },
     {
         name: "قاری عبد الحفیظ فیصل آبادی۔",
-        img: "/assets/images/ulma/17.jpg",
+        img: "/assets/images/ulma/17.JPG",
     },
     {
         name: "ڈاکٹر بابر فاروق رحیمی",
-        img: "/assets/images/ulma/18.jpg",
+        img: "/assets/images/ulma/18.JPG",
     },
     {
         name: "رانا خلیق خاں پسروری",
-        img: "/assets/images/ulma/19.jpg",
+        img: "/assets/images/ulma/19.JPG",
     },
     {
         name: "رانا شفیق خاں پسروری",
-        img: "/assets/images/ulma/20.jpg",
+        img: "/assets/images/ulma/20.JPG",
     },
     {
         name: "علامہ ابتسام الہٰی ظہیر",
-        img: "/assets/images/ulma/21.jpg",
+        img: "/assets/images/ulma/21.JPG",
     },
     {
         name: "علامہ معتصم الہٰی ظہیر",
-        img: "/assets/images/ulma/22.jpg",
+        img: "/assets/images/ulma/22.JPG",
     },
     {
         name: "قاری بنیامین عابد",
-        img: "/assets/images/ulma/23.jpg",
+        img: "/assets/images/ulma/23.JPG",
     },
     {
         name: "مفتی مبشر احمد ربانی صاحب",
-        img: "/assets/images/ulma/24.jpg",
+        img: "/assets/images/ulma/24.JPG",
     },
     {
         name: "مفتی مبشر احمد ربانی",
-        img: "/assets/images/ulma/25.jpg",
+        img: "/assets/images/ulma/25.JPG",
     },
     {
         name: "مولانا اسحاق اوکاڑوی",
-        img: "/assets/images/ulma/26.jpg",
+        img: "/assets/images/ulma/26.JPG",
     },
     {
         name: "مولانا اسماعیل عتیق",
-        img: "/assets/images/ulma/27.jpg",
+        img: "/assets/images/ulma/27.JPG",
     },
     {
         name: "مولانا ریاض الرحمان یزدانی",
-        img: "/assets/images/ulma/28.jpg",
+        img: "/assets/images/ulma/28.JPG",
     },
     {
         name: "مولانا عبدالرشید حجازی",
-        img: "/assets/images/ulma/29.jpg",
+        img: "/assets/images/ulma/29.JPG",
     },
     {
         name: "مولانا عبداللہ نثار",
-        img: "/assets/images/ulma/30.jpg",
+        img: "/assets/images/ulma/30.JPG",
     },
     {
         name: "مولانا عتیق الرحمان شاہ کشمیری",
-        img: "/assets/images/ulma/31.jpg",
+        img: "/assets/images/ulma/31.JPG",
     },
     {
         name: "مولانا نعیم بٹ",
-        img: "/assets/images/ulma/32.jpg",
+        img: "/assets/images/ulma/32.JPG",
     },
     {
         name: "مولانا یوسف قصوری",
-        img: "/assets/images/ulma/33.jpg",
+        img: "/assets/images/ulma/33.JPG",
     },
     {
         name: "مولانا یونس آزاد",
-        img: "/assets/images/ulma/34.jpg",
+        img: "/assets/images/ulma/34.JPG",
     },
 ]
