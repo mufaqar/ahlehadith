87c473e2e12be314e2215ecc6933a96343fdc77c
mufaqar <mufaqar@gmail.com>
2024-01-25 20:02:14 +0500

    httpss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images:{
-          domains:["images.pexels.com", "youtube.com", "i.ytimg.com", "revelallday.org" , "ahlehadith.paigham.tv", "ant.a59.mywebsitetransfer.com"]
-     }
+     // images:{
+     //      domains:["images.pexels.com", "youtube.com", "i.ytimg.com", "revelallday.org" , "ahlehadith.paigham.tv", "ant.a59.mywebsitetransfer.com"]
+     // }
+     images: {
+          remotePatterns: [
+            {
+              protocol: "http",
+              hostname: "**",
+            },
+            {
+               protocol: "https",
+               hostname: "**",
+             },
+          ],
+        },
 }
 
 module.exports = nextConfig
